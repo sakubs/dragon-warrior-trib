9013c36471b921d3f1acf99d548084bd4f201301
Brian Sakurada <sakuradabs@gmail.com>
2019-09-22 11:09:37 +0900

    Finished the main canvas, added sprites.

---
+++ main.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const FONT = "48px monospace";
+
+/* 内部カウンター*/
+let gFrame = 0;
+
+function WmTimer() {
+    ++gFrame;
+    /* main canvas. */
+    const ca = document.getElementById("main");
+    const g = ca.getContext("2d");
+    g.font = FONT;
+    g.fillText("Hello World" + gFrame, gFrame / 10, 64);
+}
+
+
+/* ブラウザ起動イベント */
+window.onload = function() {
+    
+    setInterval(function() {
+        /* 30.3fps */
+        WmTimer()}, 33);
+}
