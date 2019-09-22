2216debae2ed13c4239cf82bc36803fce7260bab
Brian Sakurada <sakuradabs@gmail.com>
2019-09-22 11:17:47 +0900

    Can now load and display the spritesheet.

--- main.js
+++ main.js
@@ -11,6 +11,12 @@ function WmTimer() {
     /* main canvas. */
     const ca = document.getElementById("main");
     const g = ca.getContext("2d");
+
+    for (let x = 0; x < 16; ++x) {
+        for (let y = 0; y < 16; ++y) {
+            g.drawImage(gImgMap, x * 64, y * 64);
+        }
+    }
     g.font = FONT;
     g.fillText("Hello World" + gFrame, gFrame / 10, 64);
 }
@@ -19,7 +25,8 @@ function WmTimer() {
 /* ブラウザ起動イベント */
 window.onload = function() {
     gImgMap = new Image();
-        
+    gImgMap.src = "img/tiles.png";
+
     setInterval(function() {
         /* 30.3fps */
         WmTimer()}, 33);
