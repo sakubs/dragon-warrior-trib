f948f69a76e9334d8336c924b2b493095667422b
Brian Sakurada <sakuradabs@gmail.com>
2019-09-22 15:11:53 +0900

    added image smoothing.

--- main.js
+++ main.js
@@ -4,17 +4,20 @@ const FONT = "48px monospace";
 /* 仮想画面サイズ */
 const HEIGHT = 120;
 const WIDTH = 128;
+/* 補間処理 */
+const SMOOTH = 0;
 
 /* 内部カウンター*/
 let gFrame = 0;
 let gImgMap;
 let gScreen;
-
+let gHeight;
+let gWidth;
 
 
 function drawMain(ca) {
     const g = gScreen.getContext("2d");
-    
+
     for (let y = 0; y < 15; ++y) {
         for (let x = 0; x < 24; ++x) {
             g.drawImage(gImgMap, x * 16, y  * 16);
@@ -28,16 +31,35 @@ function drawMain(ca) {
 function wmPaint(ca) {
     drawMain(ca);
     const g = ca.getContext("2d");
-    g.drawImage(gScreen, 0, 0, gScreen.width, gScreen.height, 0, 0, ca.width, ca.height);
+    gWidth = ca.width;
+    gHeight = ca.height;
+    
+    if (gWidth / WIDTH < gHeight / HEIGHT) {
+        gHeight = gWidth * HEIGHT / WIDTH;
+    } else {
+        gWidth = gHeight * WIDTH / HEIGHT;
+    }
+
+    g.drawImage(gScreen, 0, 0, gScreen.width, gScreen.height, 0, 0, 
+        gWidth, gHeight);
 }
 
 
+/* 
+ * ブラウザサイズ変更イベント。
+ */
 function wmSize(ca) {
     ca.width = window.innerWidth;
     ca.height = window.innerHeight;
+
+    const g = ca.getContext("2d");
+    g.imageSmoothingEnabled = g.msImageSmoothingEnabled = SMOOTH;
 }
 
 
+/*
+ * タイマーイベント発生時の
+ */
 function wmTimer(ca) {
     ++gFrame;
     
