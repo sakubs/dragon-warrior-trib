8e15208f00afe650fabf76d5b76152be132fb0b7
Brian Sakurada <sakuradabs@gmail.com>
2019-09-22 12:38:33 +0900

    still working on sizing. The aspect ratio is not quite ready yet.

--- main.js
+++ main.js
@@ -1,22 +1,34 @@
 "use strict";
 
 const FONT = "48px monospace";
+/* 仮想画面サイズ */
+const HEIGHT = 120;
+const WIDTH = 128;
 
 /* 内部カウンター*/
 let gFrame = 0;
 let gImgMap;
+let gScreen;
 
 
-function wmPaint(ca) {
-    const g = ca.getContext("2d");
+
+function drawMain(ca) {
+    const g = gScreen.getContext("2d");
     
-    for (let x = 3; x < 4; ++x) {
-        for (let y = 3; y < 4; ++y) {
-            g.drawImage(gImgMap, x * 16, y * 16);
+    for (let y = 0; y < 15; ++y) {
+        for (let x = 0; x < 24; ++x) {
+            g.drawImage(gImgMap, x * 16, y  * 16);
         }
     }
     g.font = FONT;
     g.fillText("Hello World" + gFrame, gFrame / 10, 64);
+}
+
+
+function wmPaint(ca) {
+    drawMain(ca);
+    const g = ca.getContext("2d");
+    g.drawImage(gScreen, 0, 0, gScreen.width, gScreen.height, 0, 0, ca.width, ca.height);
 }
 
 
@@ -40,7 +52,13 @@ window.onload = function() {
     gImgMap = new Image();
     gImgMap.src = "img/tiles.png";
 
+    /* 仮想画面を作る */
+    gScreen = document.createElement("canvas");
+    gScreen.width = WIDTH;
+    gScreen.height = HEIGHT;
+
     wmSize(ca);
+    window.addEventListener("resize", function() { wmSize(ca) });
     setInterval(function() {
         /* 30.3fps */
         wmTimer(ca)}, 33);
