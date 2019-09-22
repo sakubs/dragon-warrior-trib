4bf36fc2d951e891f8509c76cac9ff283d74e83a
Brian Sakurada <sakuradabs@gmail.com>
2019-09-22 20:27:31 +0900

    Got the map drawing funcion completed.

--- main.js
+++ main.js
@@ -1,87 +1,144 @@
 "use strict";
 
 const FONT = "48px monospace";
-/* 仮想画面サイズ */
 const HEIGHT = 120;
 const WIDTH = 128;
-/* 補間処理 */
+const MAP_WIDTH = 32;
+const MAP_HEIGHT = 32;
 const SMOOTH = 0;
+const TILE_SIZE = 8;
+const TILE_COLUMN = 4;
+const TILE_ROW = 4;
 
-/* 内部カウンター*/
-let gFrame = 0;
-let gImgMap;
-let gScreen;
-let gHeight;
-let gWidth;
+const gameMap = [
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 3, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 3, 3, 7, 7, 7, 7, 7, 7, 7, 7, 7, 6, 6, 3, 6, 3, 6, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 3, 3, 6, 6, 7, 7, 7, 2, 2, 2, 7, 7, 7, 7, 7, 7, 7, 6, 3, 0, 0, 0, 3, 3, 0, 6, 6, 6, 0, 0, 0,
+0, 0, 3, 3, 6, 6, 6, 7, 7, 2, 2, 2, 7, 7, 2, 2, 2, 7, 7, 6, 3, 3, 3, 6, 6, 3, 6,13, 6, 0, 0, 0,
+0, 3, 3,10,11, 3, 3, 6, 7, 7, 2, 2, 2, 2, 2, 2, 1, 1, 7, 6, 6, 6, 6, 6, 3, 0, 6, 6, 6, 0, 0, 0,
+0, 0, 3, 3, 3, 0, 3, 3, 3, 7, 7, 2, 2, 2, 2, 7, 7, 1, 1, 6, 6, 6, 6, 3, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 7, 7, 7, 7, 2, 7, 6, 3, 1, 3, 6, 6, 6, 3, 0, 0, 0, 3, 3, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 6, 6, 7, 2, 7, 6, 3, 1, 3, 3, 6, 6, 3, 0, 0, 0, 3, 3, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 6, 7, 7, 7, 6, 3, 1, 1, 3, 3, 6, 3, 3, 0, 0, 3, 3, 3, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 6, 6, 7, 7, 7, 6, 3, 1, 1, 3, 3, 6, 3, 3, 0, 3,12, 3, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 6, 6, 6, 7, 7, 6, 3, 1, 1, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 6, 6, 6, 6, 3, 1, 1, 1, 1, 3, 3, 3, 3, 3, 3, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 6, 6, 3, 3, 3, 3, 1, 1, 3, 3, 3, 1, 1, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 4, 5, 3, 3, 3, 6, 6, 6, 3, 3, 3, 1, 1, 1, 1, 1, 3, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 8, 9, 3, 3, 3, 6, 6, 6, 6, 3, 3, 3, 3, 3, 3, 1, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 6, 6, 6, 3, 3, 3, 3, 3, 3, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 6, 6, 6, 6, 3, 3, 3, 3, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 6, 6, 6, 6, 3, 3, 3, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 6, 6, 6, 3, 3, 3, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 6, 6, 6, 3, 6, 6, 6, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 6, 6, 3, 6, 6, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 6, 6, 3, 6, 6, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 6, 3, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 6, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,14, 6, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 6, 6, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 6, 7, 0, 0, 0, 0, 0, 0, 0, 0,
+7,15, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 7, 7, 7, 0, 0, 0, 0, 0,
+7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 7, 7, 7, 7, 7,
+];
 
+let gameWidth;
+let gameHeight;    
+let gameScreen;
+let gameFrame = 0;
+let gameImageMap;
 
-function drawMain(ca) {
-    const g = gScreen.getContext("2d");
+function drawMain() {
+    const game = gameScreen.getContext("2d");
 
-    for (let y = 0; y < 15; ++y) {
-        for (let x = 0; x < 24; ++x) {
-            g.drawImage(gImgMap, x * 16, y  * 16);
+    for (let y = 0; y < 32; y++) {
+        for (let x = 0; x < 64; x++) {
+            drawTile(
+                game, 
+                x * TILE_SIZE, 
+                y * TILE_SIZE, 
+                gameMap[y * MAP_WIDTH + x]);            
         }
     }
-    g.font = FONT;
-    g.fillText("Hello World" + gFrame, gFrame / 10, 64);
+
+    game.font = FONT;
+    game.fillText("Dragn Waryer" + gameFrame, gameFrame / 10, 64);
 }
 
 
-function wmPaint(ca) {
-    drawMain(ca);
-    const g = ca.getContext("2d");
-    gWidth = ca.width;
-    gHeight = ca.height;
-    
-    if (gWidth / WIDTH < gHeight / HEIGHT) {
-        gHeight = gWidth * HEIGHT / WIDTH;
+function drawTile(game, x, y, index) {
+    const xIndex = (index % TILE_COLUMN) * TILE_SIZE;
+    const yIndex = Math.floor(index / TILE_ROW) * TILE_SIZE;
+    game.drawImage(
+        gameImageMap, 
+        xIndex, 
+        yIndex, 
+        TILE_SIZE, 
+        TILE_SIZE, 
+        x, 
+        y, 
+        TILE_SIZE, 
+        TILE_SIZE
+    );
+}
+
+
+function wmPaint() {
+    drawMain();
+
+    const canvas = document.getElementById("main");
+    const game = canvas.getContext("2d");
+
+    game.drawImage(
+            gameScreen, 
+            0, 
+            0, 
+            gameScreen.width, 
+            gameScreen.height, 
+            0, 
+            0, 
+            gameWidth, 
+            gameHeight
+    );
+}
+
+
+function wmSize() {
+    const canvas = document.getElementById("main");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    const game = canvas.getContext("2d");
+    game.ImageSmoothingEnabled = game.msImageSmoothingEnabled = SMOOTH;
+
+    gameWidth = canvas.width;
+    gameHeight = canvas.height;
+
+    if (gameWidth / WIDTH < gameHeight / HEIGHT) {
+        gameHeight = gameWidth * HEIGHT / WIDTH;
     } else {
-        gWidth = gHeight * WIDTH / HEIGHT;
+        gameWidth = gameHeight * WIDTH / HEIGHT;
     }
-
-    g.drawImage(gScreen, 0, 0, gScreen.width, gScreen.height, 0, 0, 
-        gWidth, gHeight);
 }
 
 
-/* 
- * ブラウザサイズ変更イベント。
- */
-function wmSize(ca) {
-    ca.width = window.innerWidth;
-    ca.height = window.innerHeight;
-
-    const g = ca.getContext("2d");
-    g.imageSmoothingEnabled = g.msImageSmoothingEnabled = SMOOTH;
+function wmTimer() {
+    gameFrame++;
+    wmPaint();
 }
 
-
-/*
- * タイマーイベント発生時の
- */
-function wmTimer(ca) {
-    ++gFrame;
-    
-    wmSize(ca);
-    wmPaint(ca);
-}
-
-
-/* ブラウザ起動イベント */
 window.onload = function() {
-    const ca = document.getElementById("main");
-    gImgMap = new Image();
-    gImgMap.src = "img/tiles.png";
+    gameImageMap = new Image(); gameImageMap.src = "img/map.png";
 
-    /* 仮想画面を作る */
-    gScreen = document.createElement("canvas");
-    gScreen.width = WIDTH;
-    gScreen.height = HEIGHT;
+    gameScreen = document.createElement("canvas");
+    gameScreen.width = WIDTH;
+    gameScreen.height = HEIGHT;
 
-    wmSize(ca);
-    window.addEventListener("resize", function() { wmSize(ca) });
-    setInterval(function() {
-        /* 30.3fps */
-        wmTimer(ca)}, 33);
+    wmSize();
+    window.addEventListener("resize", function() {wmSize()});
+
+    setInterval(function() { wmTimer() }, 33);
 }
